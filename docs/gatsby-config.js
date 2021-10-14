63f9e1ff4598594600f65717ed87516aa6cfb532
Enmanuel Jarquin <enmisac@gmail.com>
2021-10-14 11:16:42 -0600

    add react app docs

---
+++ docs/gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "smooth-doc",
+      options: {
+        name: "Auth0",
+        description: "Auth0 API implementation",
+        siteUrl: "https://example.com",
+      },
+    },
+  ],
+};
